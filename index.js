dd415028573210abec3c3990b3220f8f4540cc01
Arman <hurianarmann@gmail.com>
2023-01-24 21:05:22 +0330

    Add Dark mood

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ let firstChevronDownBtn = document.querySelector('.firstChevronDownBtn')
 let secondChevronDownBtn = document.querySelector('.secondChevronDownBtn')
 let featureList = document.querySelector('.featureList')
 let companyList = document.querySelector('.companyList')
+let lableForCheckBox = document.querySelector('#checkbox')
+let check = document.querySelector('.check')
+
 
 firstChevronDownBtn.addEventListener('click',function(){
    console.log(firstChevronDownBtn.children[0].className);
@@ -29,6 +32,16 @@ secondChevronDownBtn.addEventListener('click',function(){
         secondChevronDownBtn.children[0].classList.remove('fa-chevron-up')
         secondChevronDownBtn.children[0].classList.add('fa-chevron-down')
         companyList.style.opacity = '0'
+
+    }
+})
+
+lableForCheckBox.addEventListener('click',function(){
+    if(document.body.className !== 'addClassForBody'){
+        document.body.classList.add('addClassForBody')
+        
+    }else{
+        document.body.classList.remove('addClassForBody')
 
     }
 })
