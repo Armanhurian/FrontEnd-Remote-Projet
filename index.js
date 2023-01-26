9bab37f4a6da951b356b61677fe8a211b67ef56c
Arman <hurianarmann@gmail.com>
2023-01-26 21:36:58 +0330

    Add Question Box

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 let firstChevronDownBtn = document.querySelector('.firstChevronDownBtn')
 let secondChevronDownBtn = document.querySelector('.secondChevronDownBtn')
+let firstChevronDownBtnQuestion = document.querySelector('.firstChevronDownBtnQuestion')
+let secondChevronDownBtnQuestion = document.querySelector('.secondChevronDownBtnQuestion')
+let threthChevronDownBtnQuestion = document.querySelector('.threthChevronDownBtnQuestion')
+let forthChevronDownBtnQuestion = document.querySelector('.forthChevronDownBtnQuestion')
 let featureList = document.querySelector('.featureList')
 let companyList = document.querySelector('.companyList')
 let lableForCheckBox = document.querySelector('#checkbox')
@@ -7,11 +11,12 @@ let check = document.querySelector('.check')
 
 
 firstChevronDownBtn.addEventListener('click',function(){
-   console.log(firstChevronDownBtn.children[0].className);
+
     if(firstChevronDownBtn.children[0].className === 'fa-solid fa-chevron-down'){
         
         firstChevronDownBtn.children[0].classList.remove('fa-chevron-down')
         firstChevronDownBtn.children[0].classList.add('fa-chevron-up')
+        console.log(firstChevronDownBtn.children[0].className);
         featureList.style.opacity = '0.9'
     }else if(firstChevronDownBtn.children[0].className === 'fa-solid fa-chevron-up'){
         firstChevronDownBtn.children[0].classList.remove('fa-chevron-up')
@@ -44,4 +49,75 @@ lableForCheckBox.addEventListener('click',function(){
         document.body.classList.remove('addClassForBody')
 
     }
+})
+
+firstChevronDownBtnQuestion.addEventListener('click',function(){
+    
+    if(firstChevronDownBtnQuestion.children[0].className === 'fa-solid fa-chevron-down'){
+        
+        firstChevronDownBtnQuestion.children[0].classList.remove('fa-chevron-down')
+        firstChevronDownBtnQuestion.children[0].classList.add('fa-chevron-up')
+        firstChevronDownBtnQuestion.parentElement.parentElement.children[2].style.color = '#e84393'
+        firstChevronDownBtnQuestion.parentElement.parentElement.children[2].innerHTML = 'My name is Arman Hourian and I work as the only programmer and site designer in the team'
+
+        
+    }else if(firstChevronDownBtnQuestion.children[0].className === 'fa-solid fa-chevron-up'){
+
+        firstChevronDownBtnQuestion.children[0].classList.remove('fa-chevron-up');
+        firstChevronDownBtnQuestion.children[0].classList.add('fa-chevron-down');
+        firstChevronDownBtnQuestion.parentElement.parentElement.children[2].innerHTML = ''
+    }
+})
+secondChevronDownBtnQuestion.addEventListener('click',function(){
+
+    if(secondChevronDownBtnQuestion.children[0].className === 'fa-solid fa-chevron-down'){
+        
+        secondChevronDownBtnQuestion.children[0].classList.remove('fa-chevron-down')
+        secondChevronDownBtnQuestion.children[0].classList.add('fa-chevron-up')
+        secondChevronDownBtnQuestion.parentElement.parentElement.children[2].style.color = '#e84393'
+        secondChevronDownBtnQuestion.parentElement.parentElement.children[2].innerHTML = 'As a front-end programmer, I specialize in HTML, CSS, JAVASCRIPT, and soon React framework will be added to this list.'
+ 
+        
+    }else if(secondChevronDownBtnQuestion.children[0].className === 'fa-solid fa-chevron-up'){
+
+        secondChevronDownBtnQuestion.children[0].classList.remove('fa-chevron-up');
+        secondChevronDownBtnQuestion.children[0].classList.add('fa-chevron-down');
+        secondChevronDownBtnQuestion.parentElement.parentElement.children[2].innerHTML = ''
+    }
+})
+threthChevronDownBtnQuestion.addEventListener('click',function(){
+    
+    
+        if(threthChevronDownBtnQuestion.children[0].className === 'fa-solid fa-chevron-down'){
+            
+            threthChevronDownBtnQuestion.children[0].classList.remove('fa-chevron-down')
+            threthChevronDownBtnQuestion.children[0].classList.add('fa-chevron-up')
+            threthChevronDownBtnQuestion.parentElement.parentElement.children[2].style.color = '#e84393'
+            threthChevronDownBtnQuestion.parentElement.parentElement.children[2].innerHTML = 'I think at least after 5 months'
+
+            
+        }else if(threthChevronDownBtnQuestion.children[0].className === 'fa-solid fa-chevron-up'){
+
+            threthChevronDownBtnQuestion.children[0].classList.remove('fa-chevron-up');
+            threthChevronDownBtnQuestion.children[0].classList.add('fa-chevron-down');
+            threthChevronDownBtnQuestion.parentElement.parentElement.children[2].innerHTML = ''
+        }
+    })
+    forthChevronDownBtnQuestion.addEventListener('click',function(){
+
+        
+            if(forthChevronDownBtnQuestion.children[0].className === 'fa-solid fa-chevron-down'){
+                
+                forthChevronDownBtnQuestion.children[0].classList.remove('fa-chevron-down')
+                forthChevronDownBtnQuestion.children[0].classList.add('fa-chevron-up')
+                forthChevronDownBtnQuestion.parentElement.parentElement.children[2].style.color = '#e84393'
+                forthChevronDownBtnQuestion.parentElement.parentElement.children[2].innerHTML = 'My goal is to be recognized as a senior front-end programmer in Iran and then immigrate'
+
+                
+            }else if(forthChevronDownBtnQuestion.children[0].className === 'fa-solid fa-chevron-up'){
+   
+                forthChevronDownBtnQuestion.children[0].classList.remove('fa-chevron-up');
+                forthChevronDownBtnQuestion.children[0].classList.add('fa-chevron-down');
+                forthChevronDownBtnQuestion.parentElement.parentElement.children[2].innerHTML = ''
+            }
 })
